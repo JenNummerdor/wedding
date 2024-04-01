e23440e8ecb67853ed9478bda9a8253b79d94ac3
Jen <jennifer.nummerdor@carvana.com>
2024-04-01 09:14:37 -0600

    ts update, picnic css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import hero from './images/hero.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className='App'>
+      <nav className='demo'>
+        <a href='#' className='brand'>
+          <span>Our Story</span>
         </a>
-      </header>
+        <a href='#' className='brand'>
+          <span>Travel & Stay</span>
+        </a>
+        <a href='#' className='brand'>
+          <span>Registry</span>
+        </a>
+      </nav>
+      <img src={hero} alt='hero' style={{ width: '100%', height: '100%' }} />
+      <h1>Coming soon!</h1>
     </div>
   );
 }
