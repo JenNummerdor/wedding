edee7745eb1cff602855a4941b57deec103eded6
Jen <jennifer.nummerdor@carvana.com>
2024-04-01 09:20:12 -0600

    warnings as errors is fucking lame

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import hero from './images/hero.jpg';
 
@@ -7,13 +5,13 @@ function App() {
   return (
     <div className='App'>
       <nav className='demo'>
-        <a href='#' className='brand'>
+        <a href='/' className='brand'>
           <span>Our Story</span>
         </a>
-        <a href='#' className='brand'>
+        <a href='/' className='brand'>
           <span>Travel & Stay</span>
         </a>
-        <a href='#' className='brand'>
+        <a href='/' className='brand'>
           <span>Registry</span>
         </a>
       </nav>
