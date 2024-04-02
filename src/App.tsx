f1dbb7dd017133ad8d788e2afafa8a7577cc15d0
Jen <jennifer.nummerdor@carvana.com>
2024-04-02 09:22:08 -0600

    hamburger menu, idk if I like this library

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import './App.css';
 import hero from './images/hero.jpg';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 function App() {
   return (
     <div className='App'>
-      <nav className='demo'>
+      <nav className='mainNav'>
         <a href='/' className='brand'>
-          <span>Our Story</span>
+          <span>💍 J+O</span>
         </a>
-        <a href='/' className='brand'>
-          <span>Travel & Stay</span>
-        </a>
-        <a href='/' className='brand'>
-          <span>Registry</span>
-        </a>
+        <input id='bmenub' type='checkbox' className='show' />
+        <label htmlFor='bmenub' className='burger pseudo button'>
+          <RxHamburgerMenu />
+        </label>
+
+        <div className='menu'>
+          <a href='/'>Our Story</a>
+          <a href='/'>Travel & Stay</a>
+          <a href='/'>
+            <span>Registry</span>
+          </a>
+          <a href='/'>
+            <span>RSVP</span>
+          </a>
+        </div>
       </nav>
       <img src={hero} alt='hero' style={{ width: '100%', height: '100%' }} />
       <h1>Coming soon!</h1>
