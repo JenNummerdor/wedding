b558ed14c1dd51aba06e21d2f063f7a78a6daaa8
Jen <jennifer.nummerdor@carvana.com>
2024-04-02 09:39:22 -0600

    icon change, menu items use pseudo button

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
 import './App.css';
-import hero from './images/hero.jpg';
-import { RxHamburgerMenu } from 'react-icons/rx';
+import { Hero } from './Hero';
+import { Nav } from './Nav';
 
 function App() {
   return (
     <div className='App'>
-      <nav className='mainNav'>
-        <a href='/' className='brand'>
-          <span>💍 J+O</span>
-        </a>
-        <input id='bmenub' type='checkbox' className='show' />
-        <label htmlFor='bmenub' className='burger pseudo button'>
-          <RxHamburgerMenu />
-        </label>
-
-        <div className='menu'>
-          <a href='/'>Our Story</a>
-          <a href='/'>Travel & Stay</a>
-          <a href='/'>
-            <span>Registry</span>
-          </a>
-          <a href='/'>
-            <span>RSVP</span>
-          </a>
-        </div>
-      </nav>
-      <img src={hero} alt='hero' style={{ width: '100%', height: '100%' }} />
+      <Nav />
+      <Hero />
       <h1>Coming soon!</h1>
     </div>
   );
---
+++ src/Hero.tsx
@@ -0,0 +1,9 @@
+import hero from './images/hero.jpg';
+
+export const Hero = () => {
+  return (
+    <>
+      <img src={hero} alt='hero' style={{ width: '100%', height: '100%' }} />
+    </>
+  );
+};
