ac91297664a8227de5f176414ca31e74a1f5e1d8
Oscar Sandoval <apollo.sandoval@gmail.com>
2024-04-04 17:05:15 -0600

    feat: home page layout (#12)
    * - styling for Our Story section
    
    * - draft of real 'our story'
    
    * - draft of real 'our story'
    
    * - add property map to travel page
    
    ---------
    
    Co-authored-by: oscar.sandovalreynoso <oscar.sandovalreynoso@carvana.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,29 @@ import {Hero} from "@/app/components/hero";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Hero />
-      <h1>Coming Soon!</h1>
+      <Hero/>
+      <div className="py-10" id="story-container">
+        <div className="flex flex-col items-center max-w-5xl gap-y-4">
+          <h1 className="uppercase text-xl">Our Story</h1>
+          <p className="text-base text-center">
+            Jen and Oscar met in 2016 playing in the mountains of Colorado after being introduced by their mutual friend
+            Vivian. Very soon after meeting the three of them began planning their first adventure into Utah.
+            Serendipitously, Jen and Oscar were put in charge of driving the first day and what should have been a short
+            drive turned into an all night affair after Oscar missed his exit by several hours lost in
+            conversation with Jen. One trip, turned into many and days became years...
+          </p>
+          <p className="text-base text-center">
+            These days their lives are a little busier and a little richer with the growing family and friends who
+            support
+            them. You can still find all of them playing in the mountains; and most importantly Jen and Oscar will still
+            occasionally miss their exit.
+          </p>
+          <p className="text-base text-center">
+            They are so excited for you to join them in this beautiful state they call home and cannot wait to celebrate
+            with you in August!
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
