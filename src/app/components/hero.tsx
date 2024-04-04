03a3dbbaae2c5f5fba066d2a4ffb3057c1382897
oscar.sandovalreynoso <oscar.sandovalreynoso@carvana.com>
2024-04-03 22:54:44 -0600

    - styling for Our Story section

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,3 +1,24 @@
 import Image from "next/image";
 import hero from "public/hero.jpg";
-export const Hero = () => <Image src={hero} alt="hero" style={{ width: '100%', height: '100%' }} />
+import {Card, CardFooter, CardHeader} from "@nextui-org/react";
+export function Hero() {
+  return (
+    <Card shadow="lg" radius="lg" className="max-w-5xl">
+      <CardHeader className="absolute z-10 top-1 items-center">
+        <div className="flex flex-col items-center mx-auto">
+          <p className="text-black text-4xl font-semibold">
+            Jen&nbsp;<span className="italic text-sm">and</span>&nbsp;Oscar
+          </p>
+          <p className="text-black text-lg font-medium">are getting married!</p>
+        </div>
+      </CardHeader>
+      <Image src={hero} alt="hero" style={{ width: '100%', height: '100%' }} />
+      <CardFooter className="absolute z-10 inset-x-0 bottom-0">
+        <div>
+          <p className="text-white text-xl font-medium">Come celebrate with us</p>
+          <h4 className="text-white text-4xl font-semibold">August 17, 2024</h4>
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,15 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Hero />
-      <h1>Coming Soon!</h1>
+      <div className="flex flex-col items-center gap-y-2 py-10">
+        <h1 className="uppercase text-xl">Our Story</h1>
+        <p className="text-base text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam maecenas sed enim ut sem viverra aliquet. Tristique senectus et netus et malesuada fames. Nunc lobortis mattis aliquam faucibus purus in massa tempor. Fermentum et sollicitudin ac orci phasellus. Amet luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Euismod quis viverra nibh cras. Iaculis eu non diam phasellus vestibulum lorem. Maecenas ultricies mi eget mauris. Amet nisl suscipit adipiscing bibendum. Euismod quis viverra nibh cras. Fames ac turpis egestas maecenas pharetra convallis posuere morbi. Blandit turpis cursus in hac. Leo integer malesuada nunc vel risus commodo. Id eu nisl nunc mi ipsum faucibus vitae.
+        </p>
+        <p className="text-base text-center">
+          Imperdiet nulla malesuada pellentesque elit eget gravida cum sociis. Lectus arcu bibendum at varius vel. Eget nullam non nisi est sit amet facilisis magna etiam. Pellentesque id nibh tortor id aliquet. Integer quis auctor elit sed vulputate mi sit. Elementum pulvinar etiam non quam.
+        </p>
+      </div>
     </main>
   );
 }
