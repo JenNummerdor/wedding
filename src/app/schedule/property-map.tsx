51dd254cb901153faa10f185faf91e81e6cf7ece
Jen <jennifer.nummerdor@carvana.com>
2024-04-18 11:49:29 -0600

    enable touch on mobile

--- src/app/schedule/property-map.tsx
+++ src/app/schedule/property-map.tsx
@@ -1,26 +1,41 @@
-"use client"
+'use client';
 
-import Image from "next/image";
+import Image from 'next/image';
 import map from 'public/property-map.jpg';
-import {Card, Modal, ModalBody, ModalContent, useDisclosure} from "@nextui-org/react";
+import {
+  Card,
+  Modal,
+  ModalBody,
+  ModalContent,
+  useDisclosure,
+} from '@nextui-org/react';
 
 export function PropertyMap() {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   return (
     <>
-      <Card isPressable shadow="lg" className="max-w-5xl m-4" onPress={onOpen}>
-        <Image src={map} alt="property map of whispering oaks"/>
+      <Card isPressable shadow='lg' className='max-w-5xl m-4' onPress={onOpen}>
+        <Image src={map} alt='property map of whispering oaks' />
       </Card>
-      <Modal size="4xl" placement="center" isOpen={isOpen} onOpenChange={onOpenChange}>
+      <Modal
+        size='4xl'
+        placement='center'
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+      >
         <ModalContent>
-          {_ => (
+          {(_) => (
             <ModalBody>
-              <Image src={map} alt="property map of whispering oaks"/>
+              <Image
+                className='touch-auto'
+                src={map}
+                alt='property map of whispering oaks'
+              />
             </ModalBody>
           )}
         </ModalContent>
       </Modal>
     </>
-  )
+  );
 }
